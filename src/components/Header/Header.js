00a0c0afe8eae6229c7e7d0b53de5d62acf6f481
neel123go <ayounpaul1029@gmail.com>
2022-03-30 20:40:19 +0600

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>MealDB</div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
